49c34e6ec0bcd70fb3f24a0629388eac53b651c0
Benjamin Fabri <bfabri@pm.me>
2024-07-12 11:55:28 +0200

    Add tab3 config

--- src/background/background.js
+++ src/background/background.js
@@ -1,25 +1,31 @@
-    chrome.runtime.onInstalled.addListener(function () {
-        chrome.storage.local.get(["tab1", "tab2"], function (result) {
-            if (Object.keys(result).length === 0) {
-                let defaultData = {
-                    tab1: {
-                        color: "#f47521",
-                        layout: "layout4",
-                        text: "Score",
-                        order: "order1",
-                        decimal: "decimal1",
-                    },
-                    tab2: {
-                        color: "#a0a0a0",
-                        layout: "layout3",
-                        text: "| Score",
-                        decimal: "decimal2",
-                    },
-                };
-                chrome.storage.local.set(defaultData);
-            }
-        });
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.get(["tab1", "tab2", "tab3"], function (result) {
+        if (Object.keys(result).length === 0) {
+            let defaultData = {
+                tab1: {
+                    color: "#f47521",
+                    layout: "layout4",
+                    text: "Score",
+                    order: "order1",
+                    decimal: "decimal1",
+                },
+                tab2: {
+                    color: "#a0a0a0",
+                    layout: "layout3",
+                    text: "| Score",
+                    decimal: "decimal2",
+                },
+                tab3: {
+                    color: "#a0a0a0",
+                    layout: "layout3",
+                    text: "-",
+                    decimal: "decimal2",
+                },
+            };
+            chrome.storage.local.set(defaultData);
+        }
     });
+});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.status === "complete") {
